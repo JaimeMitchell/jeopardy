26d555ec4882a45548d862eb4e1049230576e0c2
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-07-11 11:05:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    console.log('Hello from Contructor')
+    this.state = {
+      giffy: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('Hello from Component did mount')
+
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=kcZXEZbYiUw8B0PNOUcO1FIayqcbA54q&limit=25&rating=g')
+      .then(response => response.json()) // parse the request
+      // .then(json => console.log(json));
+      .then(giffyData => this.setState({ giffy: giffyData.data })) // get the data
+  }
+
+  componentDidUpdate() {
+    console.log('Hello from component did update')
+  }
+
+  render() {
+    console.log('Hello from Render')
+    return (
+      <div>
+        <h1>Lifecycle Methods</h1>
+        {this.state.giffy.map(data => <img src={data.images.original.url}/>)}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
